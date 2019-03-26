cd8e78a1c1f01560f1901869083251384222f758
Evgeniy Polyakov <zbr@ioremap.net>
2019-03-26 16:21:18 +0000

    network: use proper policy/value layers after lstm, save lstm state and use data from client to initialize it

--- network.py
+++ network.py
@@ -84,24 +84,43 @@ def create_conv_network(config, input_dict, scope='input', reuse=False):
         params = input_dict['params']
 
         conv_state = tf.concat([conv_out, clipped_reward, one_hot_action, params], axis=1)
-        logging.info('conv lyaer: conv state: {}'.format(conv_state))
+        logging.info('conv layer: conv state: {}'.format(conv_state))
 
         return conv_state
 
-def create_rnn_network(config, input_features, scope='lstmp', reuse=False):
-    with tf.variable_scope(scope, reuse=reuse):
-        logging.info('lstm lyaer: input: {}'.format(input_features))
+class rnn_head:
+    def __init__(self, config, input_features, scope='lstm', reuse=False):
+        with tf.variable_scope(scope, reuse=reuse):
+            logging.info('lstm layer: input: {}'.format(input_features))
 
-        rnn_layers = [tf.nn.rnn_cell.LSTMCell(lstm_size) for lstm_size in [8, config.get('num_actions') + 1]]
-        multi_rnn_cell = tf.nn.rnn_cell.MultiRNNCell(rnn_layers)
+            batch_size = config['batch_size']
 
-        outputs, state = tf.nn.dynamic_rnn(multi_rnn_cell, input_features, dtype=tf.float32)
+            num_lstm_outputs = 10
+            self.lstm_cell = tf.nn.rnn_cell.BasicLSTMCell(num_lstm_outputs, state_is_tuple=True)
 
-        # only get batch of one last timestamp predictions
-        #output = outputs[:, -1, :]
-        logging.info('lstm lyaer: input: {}, outputs: {}, state: {}'.format(input_features, outputs, state))
+            self.c_state_ph = tf.placeholder(tf.float32, shape=[batch_size, self.lstm_cell.state_size.c], name='input/c_state')
+            self.h_state_ph = tf.placeholder(tf.float32, shape=[batch_size, self.lstm_cell.state_size.h], name='input/h_state')
 
-        policy_logits, baseline = tf.split(outputs, [config.get('num_actions'), 1], axis=-1)
-        baseline = tf.squeeze(baseline, axis=-1)
+            _ = tf.constant((self.lstm_cell.state_size.c, self.lstm_cell.state_size.h), dtype=tf.int32, name='output/lstm_state_sizes')
 
-    return policy_logits, baseline
+            state_in = tf.nn.rnn_cell.LSTMStateTuple(self.c_state_ph, self.h_state_ph)
+
+            logging.info('c_states: {}, h_states: {}, state_in: {}'.format(self.c_state_ph, self.h_state_ph, state_in))
+
+            lstm_outputs, lstm_state = tf.nn.dynamic_rnn(self.lstm_cell, input_features, initial_state=state_in, dtype=tf.float32, time_major=False)
+            self.lstm_state = tf.identity(lstm_state, 'output/lstm_state')
+
+            logging.info('lstm_outputs: {}, lstm_state: {}'.format(lstm_outputs, lstm_state))
+
+            num_timesteps = tf.shape(lstm_outputs)[1]
+            reshaped_output_to_batch = tf.reshape(lstm_outputs, [-1, num_lstm_outputs])
+
+            batched_policy_logits = tf.layers.dense(inputs=reshaped_output_to_batch, units=config['num_actions'], activation=None, use_bias=True, name='batched_policy_logits_layer')
+            batched_baseline = tf.layers.dense(inputs=reshaped_output_to_batch, units=1, activation=None, use_bias=True, name='batched_baseline_layer')
+
+            self.policy_logits = tf.reshape(batched_policy_logits, [-1, num_timesteps, config['num_actions']])
+            self.baseline = tf.reshape(batched_baseline, [-1, num_timesteps])
+
+    def init_sizes(self):
+        return (self.lstm_cell.state_size.c, self.lstm_cell.state_size.h)
+
